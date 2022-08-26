677712590148bd52d457a6df0c39731fb0706d65
Nima Payandeh <nimapayandeh11@gmail.com>
2022-08-26 19:34:48 +0430

    create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kRedColor = Colors.redAccent;
+const kGreenColor = Color(0xff02d39a);
+const kBlackColor = Colors.black;
+const kbackgroundColor = Color(0xFF181835);
