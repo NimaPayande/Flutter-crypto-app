69c5bc609409f52f20cc0249eca8725897cf4e33
Nima Payandeh <nimapayandeh11@gmail.com>
2022-08-27 14:56:57 +0430

    add coin service

--- lib/services/coin_service.dart
+++ lib/services/coin_service.dart
@@ -1,9 +1,9 @@
 import 'package:http/http.dart' as http;
-import '../model/coin.dart';
+import '../models/coin.dart';
 
 Future<List<Coin>> fetchData() async {
   const coinUrl =
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C7d%2C14d%2C30d';
   final response = await http.get(Uri.parse(coinUrl));
   if (response.statusCode == 200) {
     return coinFromJson(response.body);
