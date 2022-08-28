fbf4ea078e8e888528fd08ba7cf9a2e3fec97c3b
Nima Payandeh <nimapayandeh11@gmail.com>
2022-08-28 16:00:35 +0430

    move background to widget/ directory and delete some unnecessary files

--- lib/models/chart_data.dart
+++
@@ -1,5 +0,0 @@
-class ChartData {
-  final num value;
-  final int hour;
-  ChartData(this.value, this.hour);
-}
--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -1,8 +1,6 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-
-import 'constants.dart';
+import '../constants.dart';
 
 class Background extends StatelessWidget {
   const Background({Key? key, required this.child}) : super(key: key);
